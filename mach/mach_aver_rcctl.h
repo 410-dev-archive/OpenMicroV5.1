2bc2a338d47525a195ea277f6f32519dcc86f399
410 <58301450+410-dev@users.noreply.github.com>
2020-09-22 13:00:20 +0900

    Create mach_aver_rcctl.h

---
+++ mach/mach_aver_rcctl.h
@@ -0,0 +1,16 @@
+/*
+
+AVER Code Write Department
+
+VEX V5 Remote Controller Receiving Controller
+
+*/
+
+#include "vex.h"
+
+using namespace vex;
+
+class AERemoteControl {
+public:
+	// Dont code here yet
+}
