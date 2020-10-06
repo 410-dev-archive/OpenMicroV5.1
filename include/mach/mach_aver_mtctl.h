c1b4bd87c0f4818507840b720f75a075e038236b
410 <58301450+410-dev@users.noreply.github.com>
2020-10-06 17:05:40 +0900

    Added motor controller

---
+++ include/mach/mach_aver_mtctl.h
@@ -0,0 +1,29 @@
+/*
+
+AVER Code Write Department
+
+VEX V5 Brain Screen Controller
+
+*/
+
+#include "vex.h"
+
+using namespace vex;
+using namespace std;
+
+class AEMotorController {
+private:
+	controller ctl = controller();
+public:
+	int runMotor(motor[] motorObjects, int timeInMillisecond) {
+		try{
+			for(int indexOfObjects = 0; indexOfObjects < motorObjects.length; indexOfObjects++) {
+				motorObjects[indexOfObjects].spin(directionType::fwd, ctl.Axis3.value(),velocityUnits::pct);
+			}
+			task::sleep(timeInMillisecond);
+			return 0;
+		}catch(Exception e) {
+			return 1;
+		}
+	}
+}
