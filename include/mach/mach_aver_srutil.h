4c58e6eb7604285713c90f2d970e5c3ac32d87d4
410 <58301450+410-dev@users.noreply.github.com>
2020-10-06 17:05:48 +0900

    Added sensor utilites

---
+++ include/mach/mach_aver_srutil.h
@@ -0,0 +1,18 @@
+/*
+
+AVER Code Write Department
+
+VEX V5 Sensor Utilities
+
+*/
+
+#include "vex.h"
+
+using namespace vex;
+
+class AESensorsUtility {
+public:
+	int getShaftEncoderValue() {
+		
+	}
+}
