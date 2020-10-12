84662af3a73cc2411d5bc71d29c585ffa8087776
410 <58301450+410-dev@users.noreply.github.com>
2020-10-12 20:16:22 +0900

    Better log + commenting

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ competition Competition;
 vex::controller Controller;
 AEMotorControl motorctl;
 
+// Method for async (vex::thread)
 void motorCall() {
   motor motorArray[] = {TEST};
   motorctl.runMotor(motorArray, 1, 5000);
@@ -38,19 +39,23 @@ int main(){
   AEScreenControl scctl;
   scctl.init();
   scctl.clearScreen();
-  scctl.setValueOfLine(1, 0, "Team AVER Mach Core - Test Version 0.0.1");
-  scctl.setValueOfLine(2, 0, "========================================");
-  scctl.setValueOfLine(3, 0, "Code should run now.");
-
-  short lengthOfPrefix_ShaftEncoder = scctl.setLinePrefix(4, "Shaft Encoder Value: ");
-
+  scctl.setValueOfLine(1, 0, "Team AVER Mach - Test Version 0.0.1");
+  scctl.setValueOfLine(2, 0, "===================================");
+  scctl.setValueOfLine(3, 0, "[LOG] Code should run now.");
+  scctl.setValueOfLine(4, 0, "[LOG] Starting mach_aver_motor in async thread.");
   vex::thread newTask(motorCall); // Motor Async Run
+
+  // Create Object  
   AESensorsUtility sensorutil;
 
+  // Sets Encoder Value Prefix
+  short lengthOfPrefix_ShaftEncoder = scctl.setLinePrefix(5, "Shaft Encoder Value: ");
+
+  // Keep receives value from Shaft Encoder until motorctl.motorStatus is true.
   while(motorctl.isMotorSessionRunning()) {
+    // Converts returned shaft encoder value to string and sets the value of line
     stringstream sstream;
-    // sstream << "Hello World";
     sstream << (sensorutil.getShaftEncoderValue(EncoderA), "");
-    scctl.setValueOfLine(4, lengthOfPrefix_ShaftEncoder, sstream.str());
+    scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, sstream.str());
   }
 }
