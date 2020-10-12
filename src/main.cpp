9dfb1a0f3d81022096aa36811ef8610018de7535
410 <58301450+410-dev@users.noreply.github.com>
2020-10-12 20:07:55 +0900

    Added motor / sensor control

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ using namespace std;
 
 competition Competition;
 vex::controller Controller;
+AEMotorControl motorctl;
 
 // pre-autonomous code
 void pre_auton(void){
@@ -44,9 +45,31 @@ void usercontrol(void){
   while (true){
     wait(20, msec);
   }
+void motorCall() {
+  motor motorArray[] = {TEST};
+  motorctl.runMotor(motorArray, 1, 5000);
 }
 
 // main
 int main(){
   usercontrol();
+  // Sensor Test - Displayed on Brain
+  AEScreenControl scctl;
+  scctl.init();
+  scctl.clearScreen();
+  scctl.setValueOfLine(1, 0, "Team AVER Mach Core - Test Version 0.0.1");
+  scctl.setValueOfLine(2, 0, "========================================");
+  scctl.setValueOfLine(3, 0, "Code should run now.");
+
+  short lengthOfPrefix_ShaftEncoder = scctl.setLinePrefix(4, "Shaft Encoder Value: ");
+
+  vex::thread newTask(motorCall); // Motor Async Run
+  AESensorsUtility sensorutil;
+
+  while(motorctl.isMotorSessionRunning()) {
+    stringstream sstream;
+    // sstream << "Hello World";
+    sstream << (sensorutil.getShaftEncoderValue(EncoderA), "");
+    scctl.setValueOfLine(4, lengthOfPrefix_ShaftEncoder, sstream.str());
+  }
 }
