27ffee368a0200cda2ec609319aeab612faac8af
410 <58301450+410-dev@users.noreply.github.com>
2020-10-13 17:12:37 +0900

    Shrinked

--- src/main.cpp
+++ src/main.cpp
@@ -12,50 +12,37 @@
 
 #include "vex.h"
 #include "hardwareinfo.h"
-#include "mach/mach_aver_fctl.h"
-#include "mach/mach_aver_scctl.h"
-#include "mach/mach_aver_srutil.h"
-#include "mach/mach_aver_mtctl.h"
-#include <iostream>
-#include <string>
-#include <sstream>
+
+#include "RSManager/main.h"
 
 using namespace vex;
 using namespace std;
 
 competition Competition;
 vex::controller Controller;
-AEMotorControl motorctl;
+RSManager System;
 
 // Method for async (vex::thread)
 void motorCall() {
   motor motorArray[] = {TEST};
-  motorctl.runMotor(motorArray, 1, 5000);
+  System.mctl.runMotor(motorArray, 1, 5000);
 }
 
 // main
 int main(){
-  // Sensor Test - Displayed on Brain
-  AEScreenControl scctl;
-  scctl.init();
-  scctl.clearScreen();
-  scctl.setValueOfLine(1, 0, "Team AVER Mach - Test Version 0.0.1");
-  scctl.setValueOfLine(2, 0, "===================================");
-  scctl.setValueOfLine(3, 0, "[LOG] Code should run now.");
-  scctl.setValueOfLine(4, 0, "[LOG] Starting mach_aver_motor in async thread.");
+  // Launches Resource Manager
+  System.init();
+
   vex::thread newTask(motorCall); // Motor Async Run
 
-  // Create Object  
-  AESensorsUtility sensorutil;
-
   // Sets Encoder Value Prefix
-  short lengthOfPrefix_ShaftEncoder = scctl.setLinePrefix(5, "Shaft Encoder Value: ");
+  short lengthOfPrefix_ShaftEncoder = System.scctl.setLinePrefix(5, "Shaft Encoder Value: ");
 
   // Keep receives value from Shaft Encoder until motorctl.motorStatus is true.
-  while(motorctl.isMotorSessionRunning()) {
+  while(System.mctl.isMotorSessionRunning()) {
     // Converts returned shaft encoder value to string and sets the value of line
     stringstream sstream;
-    sstream << (sensorutil.getShaftEncoderValue(EncoderA), "");
-    scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, sstream.str());
+    sstream << (System.srutil.getShaftEncoderValue(EncoderA), "");
+    System.scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, sstream.str());
   }
 }
