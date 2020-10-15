e81c70f5b2fac7fc76a97b21c0056407d3885678
410 <58301450+410-dev@users.noreply.github.com>
2020-10-15 17:26:55 +0900

    ~

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 
 competition Competition;
 controller Controller;
-RSManager System;
+RSManager System("");
 
 // Method for async (vex::thread)
 void motorCall() {
@@ -31,14 +31,17 @@ void motorCall() {
 int main(){
   // Launches Resource Manager
   System.init();
-  vex::thread newTask(motorCall); // Motor Async Run
+  // vex::thread newTask(motorCall); // Motor Async Run
 
+  System.status("Motor running...");
+  System.mctl.runSingleMotor(TEST, 3000);
+  System.status("Motor stopped.");
   // Sets Encoder Value Prefix
   short lengthOfPrefix_ShaftEncoder = System.scctl.setLinePrefix(5, "Shaft Encoder Value: ");
 
   // Keep receives value from Shaft Encoder until motorctl.motorStatus is true.
-  while(System.mctl.isMotorSessionRunning()) {
-    // Converts returned shaft encoder value to string and sets the value of line
-    System.scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, System.convertToString((double) System.srutil.getShaftEncoderValue(EncoderA)));
-  }
+  // while(System.mctl.isMotorSessionRunning()) {
+  //   // Converts returned shaft encoder value to string and sets the value of line
+  //   System.scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, System.convertToString((double) System.srutil.getShaftEncoderValue(EncoderA)));
+  // }
 }
