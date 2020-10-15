76d6270cea81ed69f4e98e8f72a185c838baa646
410 <58301450+410-dev@users.noreply.github.com>
2020-10-15 22:11:33 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
+#include <future>
+
 #include "hardwareinfo.h"
 
 #include "RSManager/main.h"
@@ -22,9 +24,11 @@ competition Competition;
 controller Controller;
 RSManager System("");
 
-// Method for async (vex::thread)
-void motorCall() {
-  System.mctl.runMotor(WHEEL_FBMOVEMENT, 1, 5000);
+void async_task() {
+  System.status("Started async function.");
+  task::sleep(1000);
+  System.status("Motor stop!");
+  System.mctl.stopSingleMotor(TEST);
 }
 
 // main
@@ -36,8 +40,13 @@ int main(){
   System.status("Started motor!");
   System.mctl.runSingleMotor(TEST);
   System.status("Shaft encoder should work.");
+
   // Sets Encoder Value Prefix
   short lengthOfPrefix_ShaftEncoder = System.scctl.setLinePrefix(5, "Shaft Encoder Value: ");
+  short lengthOfPrefix_VisionSensor = System.scctl.setLinePrefix(6, "Vision Sensor Value: ");
+  System.status("Finished setting livedisplay prefix..");
+
+  // Create vision sensor data
   /* 여기에서 빨간색 인식하게 코드좀 짜줘 */
   int yuvData[] = {0, 0, 10, 5, 0, 100, 50, 0, 50};
   int camData[] = {0, 0, 0};
@@ -46,5 +55,6 @@ int main(){
   while(System.mctl.motorStatus) {
     // Converts returned shaft encoder value to string and sets the value of line
     System.scctl.setValueOfLine(5, lengthOfPrefix_ShaftEncoder, System.convertToString(testShaftEncoder.value()));
+    System.scctl.setValueOfLine(6, lengthOfPrefix_VisionSensor, System.convertToString(System.srutil.visionSensor(testVisionSensor, yuvData, 2.0, camData)));
   }
 }
