000f24ab333ea7c018cc11a9dd37916ec126b0de
Eric-1 <eric-1@Macintosh.local>
2020-10-19 17:46:44 +0900

    control heades 2

--- include/mach/mach_aver_ctrl.h
+++ include/mach/mach_aver_ctrl.h
@@ -25,24 +25,30 @@ public:
   AESensorsUtility aeutils;
   int buttonR1, buttonR2, buttonL1, buttonL2;
   int buttonX, buttonY, buttonA, buttonB;
-  int buttonUp, buttonDown, buttonRight
-	bool retrieve(controller Controller) {
-    int value = aeutils.getShaftEncoderValue(testShaftEncoder);
-    Controller.ButtonR1.pressing();
-    Controller.ButtonR2.pressing();
-    Controller.ButtonL1.pressing();
-    Controller.ButtonL2.pressing();
-    Controller.ButtonX.pressing();
-    Controller.ButtonY.pressing();
-    Controller.ButtonA.pressing();
-    Controller.ButtonB.pressing();
-    Controller.ButtonUp.pressing();
-    Controller.ButtonDown.pressing();
-    Controller.ButtonRight.pressing();
-    Controller.ButtonLeft.pressing();
-    Controller.Axis1.value();
-    Controller.Axis2.value();
-    Controller.Axis3.value();
-    Controller.Axis4.value();
+  int buttonUp, buttonDown, buttonRight, buttonLeft;
+  int ax1, ax2, ax3, ax4;
+  int encLeft, encRight, encBack;
+
+	void updateAll(controller Controller) {
+    encLeft = aeutils.getShaftEncoderValue(ENCODER_LEFT);
+    encRight = aeutils.getShaftEncoderValue(ENCODER_RIGHT);
+    encBack = aeutils.getShaftEncoderValue(ENCODER_BACK);
+    
+    buttonR1 = Controller.ButtonR1.pressing();
+    buttonR2 = Controller.ButtonR2.pressing();
+    buttonL1 = Controller.ButtonL1.pressing();
+    buttonL2 = Controller.ButtonL2.pressing();
+    buttonX = Controller.ButtonX.pressing();
+    buttonY = Controller.ButtonY.pressing();
+    buttonA = Controller.ButtonA.pressing();
+    buttonB = Controller.ButtonB.pressing();
+    buttonUp = Controller.ButtonUp.pressing();
+    buttonDown = Controller.ButtonDown.pressing();
+    buttonRight = Controller.ButtonRight.pressing();
+    buttonLeft = Controller.ButtonLeft.pressing();
+    ax1 = Controller.Axis1.value();
+    ax2 = Controller.Axis2.value();
+    ax3 = Controller.Axis3.value();
+    ax4 = Controller.Axis4.value();
   }
 };
